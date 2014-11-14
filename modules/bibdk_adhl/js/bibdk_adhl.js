937d303bb04d7d6209320d1439fc08ab299c3cf2
Martin Møller <mmj@dbc.dk>
2014-11-14 10:41:28 +0100

    cleanup

--- modules/bibdk_adhl/js/bibdk_adhl.js
+++ modules/bibdk_adhl/js/bibdk_adhl.js
@@ -1,9 +1,6 @@
 (function($) {
-  var Bibdk_adhl = {};
-
   /** Insert adhl results */
-  Bibdk_adhl.addRecommendation = function(adhl) {
-    console.log(adhl);
+  function addRecommendation(adhl) {
     if(adhl.error) {
       $('.recommendation-load[data-pid="' + adhl.pid + '"]')
         .parent()
@@ -16,32 +13,33 @@
       $('.recommendation-load[data-pid="' + adhl.pid + '"]').replaceWith(adhl.list);
       $('.recommendation-more[data-pid="' + adhl.pid + '"]').removeClass('visuallyhidden');
     }
-  },
-    Bibdk_adhl.loadRecommendation = function(element) {
-      var pid = $(element).attr('data-pid');
-      var isbn = $(element).attr('data-isbn');
-      /* Add throbber*/
-      $(element).addClass('ajax-progress');
-      $(element).html('<span class="throbber">&nbsp;</span>');
+  };
 
-      /* Call ajax */
-      var request = $.ajax({
-        url: Drupal.settings.basePath + 'adhl/ajax',
-        type: 'POST',
-        data: {
-          pid: pid
-        },
-        dataType: 'json',
-        success: Bibdk_adhl.addRecommendation
-      });
-    };
+  function loadRecommendation(element) {
+    var pid = $(element).attr('data-pid');
+    var isbn = $(element).attr('data-isbn');
+    /* Add throbber*/
+    $(element).addClass('ajax-progress');
+    $(element).html('<span class="throbber">&nbsp;</span>');
+
+    /* Call ajax */
+    var request = $.ajax({
+      url: Drupal.settings.basePath + 'adhl/ajax',
+      type: 'POST',
+      data: {
+        pid: pid
+      },
+      dataType: 'json',
+      success: addRecommendation
+    });
+  };
 
   /** Get holdingstatus via ajax */
   Drupal.behaviors.recommendation = {
     attach: function(context) {
       $('.field-type-worktabs .bibdk-tabs', context).one("click", function() {
         $('.recommendation-load', context).each(function(i, element) {
-          Bibdk_adhl.loadRecommendation(element);
+          loadRecommendation(element);
         });
       });
     }
