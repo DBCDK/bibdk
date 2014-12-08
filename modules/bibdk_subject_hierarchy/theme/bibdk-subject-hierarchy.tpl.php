0dc5abf5f7c7458e20a9f5929f0bafed0743893b
pjohans <pjohans@gmail.com>
2014-12-08 12:32:46 +0100

    let subject_hierarchy render its own header for frontpage

--- modules/bibdk_subject_hierarchy/theme/bibdk-subject-hierarchy.tpl.php
+++ modules/bibdk_subject_hierarchy/theme/bibdk-subject-hierarchy.tpl.php
@@ -6,29 +6,29 @@
  */
 ?>
 
-<div id="bibdk-subject-hierarchy" class="themes clearfix">
-  <div class="container clearfix">
-    <div class="clearfix">
-      <div class="bibdk-subject-hierarchy-header">
-        <?php print $header; ?>
-      </div>
-      <div class="bibdk-subject-hierarchy-helptext">
-        <p class="helptext popover-button" title="<?php print $helptext; ?>">
-          <a href="#" tabindex="0" aria-label="<?php print t('Help', array(), array('context' => 'bibdk_theme')); ?>" role="button">?</a>
+<div class="container clearfix">
+  <div class="clearfix bibdk-subject-hierarchy-panel-header">
+    <div class="bibdk-subject-hierarchy-header">
+      <?php print $header; ?>
+    </div>
+    <div class="bibdk-subject-hierarchy-helptext">
+      <p class="helptext popover-button" title="<?php print $helptext; ?>">
+        <a href="#" tabindex="0" aria-label="<?php print t('Help', array(), array('context' => 'bibdk_theme')); ?>" role="button">?</a>
+      </p>
+      <div class="popover element-wrapper linkme-wrapper visuallyhidden hide-text">
+        <p class="user-msg ">
+          <?php print $helptext; ?>
         </p>
-        <div class="popover element-wrapper linkme-wrapper visuallyhidden hide-text">
-          <p class="user-msg ">
-            <?php print $helptext; ?>
-          </p>
-          <a href="#"  tabindex="0" aria-label="<?php print t('Close help', array(), array('context' => 'bibdk_theme')); ?>" 
-             role="button" class="close icon icon-left icon-red-x "> 
-          </a>
-        </div>
-      </div>
-      <div class="subject-hierarchy-searchfield">
-        <?php print drupal_render($searchfield); ?>
+        <a href="#"  tabindex="0" aria-label="<?php print t('Close help', array(), array('context' => 'bibdk_theme')); ?>"
+           role="button" class="close icon icon-left icon-red-x ">
+        </a>
       </div>
     </div>
+    <div class="subject-hierarchy-searchfield">
+      <?php print drupal_render($searchfield); ?>
+    </div>
+  </div>
+<div id="bibdk-subject-hierarchy" class="clearfix">
     <div id="bibdk-subject-hierarchy-content">
     <?php foreach ($rows as $row) : ?>
       <?php print $row; ?>
