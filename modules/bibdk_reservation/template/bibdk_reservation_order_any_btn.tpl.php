770c0f8e35485ae4b532d010e63abf66282c5010
JrgenGNielsen <jgn@dbc.dk>
2014-12-08 12:37:00 +0100

    redo "any edition" table as render array

--- modules/bibdk_reservation/template/bibdk_reservation_order_any_btn.tpl.php
+++ modules/bibdk_reservation/template/bibdk_reservation_order_any_btn.tpl.php
@@ -8,7 +8,7 @@
     <div class="dropdown-wrapper">
       <a <?php print drupal_attributes($addi_attributes);?> class="btn btn-blue dropdown-toggle" href="#"><?php print t('Order any edition', array(), array('context'=>'bibdk_reservation')); ?> <span class="icon icon-right icon-white-down">&nbsp;</span></a>
       <div class="dropdown-menu order-any-btn-list hidden">
-        <?php print $table; ?>
+        <?php print drupal_render($table); ?>
         <div class="reservation-btn-infotext <?php print $hide; ?>"><?php print drupal_render($infotext); ?></div>
       </div>
     </div>
