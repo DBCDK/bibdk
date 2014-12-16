dcf3e2b3ddfbb2fa2f10c4d78d74d1c377a659c4
Martin Møller <mmj@dbc.dk>
2014-12-16 15:12:52 +0100

    removed counter

--- modules/bibdk_vejviser/theme/bibdk_vejviser_header.tpl.php
+++ modules/bibdk_vejviser/theme/bibdk_vejviser_header.tpl.php
@@ -1,6 +1,5 @@
 <div id="bibdk-vejviser-header">
-  <div id="bibdk-vejviser-count"><?php print t('@count records', array('@count' => $count), array('context' => 'bibdk_vejviser')); ?></div>
-  <?php print $search_form; ?>
+  <?php print $form; ?>
   <div class="bibdk-vejviser-help-text">
     <?php print t($ting_info_holder); ?><br/>
   </div>
