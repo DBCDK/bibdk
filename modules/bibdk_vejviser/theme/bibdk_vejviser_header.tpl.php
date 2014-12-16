e7fb2efae86b2f4af655de823953a7778a7b0eb7
Martin Møller <mmj@dbc.dk>
2014-12-16 13:09:36 +0100

    cleanup

--- modules/bibdk_vejviser/theme/bibdk_vejviser_header.tpl.php
+++ modules/bibdk_vejviser/theme/bibdk_vejviser_header.tpl.php
@@ -1,13 +1,8 @@
-<?php
-/**
- *
- */
-?>
 <div id="bibdk-vejviser-header">
   <div id="bibdk-vejviser-count"><?php print t('@count records', array('@count' => $count), array('context' => 'bibdk_vejviser')); ?></div>
   <?php print $search_form; ?>
   <div class="bibdk-vejviser-help-text">
-  <?php print t($ting_info_holder); ?><br/>
+    <?php print t($ting_info_holder); ?><br/>
   </div>
 </div>
 
