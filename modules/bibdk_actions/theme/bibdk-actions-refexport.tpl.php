dbdc10b53281b8763effdabca57fbc04b04b760d
Søren Vibjerg <vibjerg@gmail.com>
2015-01-07 09:59:18 +0100

    Show export options using foundations dropdown

--- modules/bibdk_actions/theme/bibdk-actions-refexport.tpl.php
+++ modules/bibdk_actions/theme/bibdk-actions-refexport.tpl.php
@@ -1,22 +1,15 @@
 <?php
 /* This template will toogle between show/hide 'export links'
- * Calls script.js toggle-next-section 
+ * Uses foundations dropdown to expand options
  */
+
 ?>
 <div class="action_element_refexportlinks">
-  <div class="element-section">
-    <div class="toggle-next-section">
-      <a class="show-more" href="#">
-        <span class="icon icon-left icon-blue-down">&nbsp;</span>
-        <?php print t('refexport_more', array(), array('context' => 'bibdk_actions')); ?>
-      </a>
-      <a class="show-less visuallyhidden" href="#">
-        <span class="icon icon-left icon-blue-up">&nbsp;</span>
-        <?php print t('refexport_less', array(), array('context' => 'bibdk_actions')); ?>
-      </a>
-    </div>
-  </div>
-  <div class="element-section visuallyhidden">
+  <a class='refexport-toggle-link' data-dropdown="<?php print $export_id; ?>" aria-controls="<?php print $export_id; ?>" aria-expanded="false">
+    <?php print t('refexport_more', array(), array('context' => 'bibdk_actions')); ?>
+  </a>
+
+  <div id="<?php print $export_id; ?>" class="f-dropdown" data-dropdown-content aria-hidden="true" tabindex="-1">
     <?php print drupal_render($linktext); ?>
   </div>
 </div>
