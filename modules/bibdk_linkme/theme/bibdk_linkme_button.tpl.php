3026cac41672bffdf5b2f281b6a3857542fe07bc
Søren Vibjerg <vibjerg@gmail.com>
2015-01-07 10:50:04 +0100

    move template code to module

--- modules/bibdk_linkme/theme/bibdk_linkme_button.tpl.php
+++ modules/bibdk_linkme/theme/bibdk_linkme_button.tpl.php
@@ -1,1 +1,11 @@
-<?php print l($link_text, $path, array('query' => $query, 'attributes' => $link_attributes)); ?>
+<div>
+  <a href="#<?php print $path; ?>" class="popover-button text-small" id="<?php print $selid ?>">
+    <span class="icon icon-left icon-lightgrey-link">&nbsp;</span>
+    <?php print $link_text; ?>
+  </a>
+
+  <div class='popover element-wrapper visuallyhidden linkme-wrapper hide-text'>
+    <div class=''><?php print drupal_render_children($link_form) ?><span
+        class="close icon icon-left icon-red-x">&nbsp;</span></div>
+  </div>
+</div>
