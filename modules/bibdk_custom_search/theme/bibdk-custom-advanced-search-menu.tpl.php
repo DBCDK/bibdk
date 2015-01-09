0edf77908337f9bb1cb7458f4e3425ad8fc1770a
Martin Møller <mmj@dbc.dk>
2015-01-09 14:41:21 +0100

    - moved tpl from b_theme to b_custom_search - refactored logic out of tpl

--- modules/bibdk_custom_search/theme/bibdk-custom-advanced-search-menu.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-advanced-search-menu.tpl.php
@@ -1,9 +1,11 @@
-<div id="search-advanced-toggle" data-toggle-state-hidden="<?php echo $form['#visuallyhidden']; ?>" class="clearfix <?php print $toggled; ?>">
-  <a id="selid_custom_search_expand" class="text-darkgrey" href="#">
-    <span class="icon icon-left icon-blue-minus">&nbsp;</span><?php print t('Expand search options'); ?>
+<div id="search-advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="clearfix <?php print $form['#toggled']; ?>">
+  <a id="selid_custom_search_expand" class="text-darkgrey <?php print $form['#toggled']; ?>" href="#">
+    <span class="icon icon-left icon-blue-minus">&nbsp;</span>
+    <span class="toggle-text <?php print $form['#expand_hidden']; ?>"><?php print t('Expand search options'); ?></span>
+    <span class="toggle-text <?php print $form['#collaps_hidden']; ?>"><?php print t('Collapse search options'); ?></span>
   </a>
 </div>
-<div id="search-advanced" class="clearfix <?php print $hidden; ?>">
+<div id="search-advanced" class="clearfix <?php print $form['#expand_hidden']; ?>">
   <div id="search-advanced-panel" class="clearfix">
     <!-- ADVANCED SEARCH-->
     <?php print drupal_render_children($form); ?>
