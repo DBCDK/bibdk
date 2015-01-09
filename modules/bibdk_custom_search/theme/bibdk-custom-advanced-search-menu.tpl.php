1a531e5bb9c17dd1eda862e7b0bdf2ebcd4ec310
Martin Møller <mmj@dbc.dk>
2015-01-09 13:54:36 +0100

    Cleaned up tpl and added a couple of vars to render array to avoid logic in tpl

--- modules/bibdk_custom_search/theme/bibdk-custom-advanced-search-menu.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-advanced-search-menu.tpl.php
@@ -1,16 +1,9 @@
-<?php
-$search_advanced_toggle = $search_advanced_visuallyhidden = '';
-if (empty($form['#visuallyhidden'])) {
-  $search_advanced_toggle = ' toggled';
-  $search_advanced_visuallyhidden = ' hidden';
-}
-?>
-<div id="search-advanced-toggle" data-toggle-state-hidden="<?php echo $form['#visuallyhidden']; ?>" class="clearfix<?php echo $search_advanced_toggle; ?>">
+<div id="search-advanced-toggle" data-toggle-state-hidden="<?php echo $form['#visuallyhidden']; ?>" class="clearfix <?php print $toggled; ?>">
   <a id="selid_custom_search_expand" class="text-darkgrey" href="#">
     <span class="icon icon-left icon-blue-minus">&nbsp;</span><?php print t('Expand search options'); ?>
   </a>
 </div>
-<div id="search-advanced" class="clearfix<?php echo $search_advanced_visuallyhidden; ?>">
+<div id="search-advanced" class="clearfix <?php print $hidden; ?>">
   <div id="search-advanced-panel" class="clearfix">
     <!-- ADVANCED SEARCH-->
     <?php print drupal_render_children($form); ?>
