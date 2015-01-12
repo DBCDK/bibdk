6501b618966536ea395c7badad0e63ec58567525
Martin Møller <mmj@dbc.dk>
2015-01-12 15:52:50 +0100

    changed render element to form

--- modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
@@ -1,14 +1,14 @@
-<div id="search-advanced-toggle" data-toggle-state-hidden="<?php print $toggled; ?>" class="clearfix <?php print $toggled; ?>">
-  <a id="selid_custom_search_expand" class="text-darkgrey <?php print $toggled; ?>" href="#">
+<div id="search-advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="clearfix <?php print $form['#toggled'];; ?>">
+  <a id="selid_custom_search_expand" class="text-darkgrey <?php print $form['#toggled']; ?>" href="#">
     <span class="icon icon-left icon-blue-minus">&nbsp;</span>
-    <span class="toggle-text <?php print $expand_hidden; ?>"><?php print t('Expand search options'); ?></span>
-    <span class="toggle-text <?php print $collaps_hidden; ?>"><?php print t('Collapse search options'); ?></span>
+    <span class="toggle-text <?php print $form['#expand_hidden']; ?>"><?php print t('Expand search options'); ?></span>
+    <span class="toggle-text <?php print $form['#collaps_hidden']; ?>"><?php print t('Collapse search options'); ?></span>
   </a>
 </div>
-<div id="search-advanced" class="clearfix <?php print $collaps_hidden; ?>">
+<div id="search-advanced" class="clearfix <?php print $form['#collaps_hidden']; ?>">
   <div id="search-advanced-panel" class="clearfix">
     <!-- ADVANCED SEARCH-->
-    <?php print $advanced_search_panel; ?>
+    <?php print drupal_render_children($form); ?>
     <!-- END ADVANCED SEARCH-->
   </div>
 </div>
