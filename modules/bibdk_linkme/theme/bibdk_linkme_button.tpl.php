83d00c2965b3446123b5a6b8908006c172ee5513
Søren Vibjerg <vibjerg@gmail.com>
2015-01-13 08:59:54 +0100

    convert link button to foundation dropdown

--- modules/bibdk_linkme/theme/bibdk_linkme_button.tpl.php
+++ modules/bibdk_linkme/theme/bibdk_linkme_button.tpl.php
@@ -1,10 +1,10 @@
-<div>
-  <a href="#<?php print $path; ?>" class="popover-button text-small" id="<?php print $selid ?>">
+<div class="linkme-wrapper">
+  <a href="#<?php print $path; ?>" class="linkme-button js-linkme-dropdown" data-dropdown="<?php print $selid ?>">
     <span class="icon icon-left icon-lightgrey-link">&nbsp;</span>
     <?php print $link_text; ?>
   </a>
 
-  <div class='popover element-wrapper visuallyhidden linkme-wrapper hide-text'>
+  <div id="<?php print $selid ?>" class="linkme-content bibdk-dropdown" data-dropdown-content aria-hidden="true" tabindex="-1">
     <div class=''><?php print drupal_render_children($link_form) ?><span
         class="close icon icon-left icon-red-x">&nbsp;</span></div>
   </div>
