8edb79f1c73bd04829f423e144d66312775f17da
Martin Møller <mmj@dbc.dk>
2015-01-13 12:55:40 +0100

    - removed unused template - added use of SVG

--- modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
@@ -1,8 +1,19 @@
 <div id="search-advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="clearfix <?php print $form['#toggled']; ?>">
   <a id="selid_custom_search_expand" class="text-darkgrey <?php print $form['#toggled']; ?>" href="#">
-    <span class="icon icon-left icon-blue-minus">&nbsp;</span>
-    <span class="toggle-text <?php print $form['#expand_hidden']; ?>"><?php print t('Expand search options'); ?></span>
-    <span class="toggle-text <?php print $form['#collaps_hidden']; ?>"><?php print t('Collapse search options'); ?></span>
+    <span class="toggle-text <?php print $form['#expand_hidden']; ?>">
+      <svg class="svg-icon svg-plus-circle">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-plus-circle"></use>
+      </svg>
+      <?php print t('Expand search options'); ?>
+    </span>
+
+    <span class="toggle-text <?php print $form['#collaps_hidden']; ?>">
+      <svg class="svg-icon svg-plus-circle">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-plus-circle"></use>
+      </svg>
+      <?php print t('Collapse search options'); ?>
+    </span>
+
   </a>
 </div>
 <div id="search-advanced" class="clearfix <?php print $form['#collaps_hidden']; ?>">
