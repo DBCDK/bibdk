eb428cda706dd30598875b7bf1104907565879be
Martin Møller <mmj@dbc.dk>
2015-01-13 13:24:29 +0100

    - removed unused class - added 'minus' svg

--- modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
@@ -1,15 +1,15 @@
 <div id="search-advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="clearfix <?php print $form['#toggled']; ?>">
   <a id="selid_custom_search_expand" class="text-darkgrey <?php print $form['#toggled']; ?>" href="#">
-    <span class="toggle-text <?php print $form['#expand_hidden']; ?>">
-      <svg class="svg-icon svg-plus-circle">
+    <span class="toggle-text">
+      <svg class="custom-search--expand-toggle-icon">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-plus-circle"></use>
       </svg>
       <?php print t('Expand search options'); ?>
     </span>
 
     <span class="toggle-text <?php print $form['#collaps_hidden']; ?>">
-      <svg class="svg-icon svg-plus-circle">
-        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-plus-circle"></use>
+      <svg class="custom-search--expand-toggle-icon">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-minus-circle"></use>
       </svg>
       <?php print t('Collapse search options'); ?>
     </span>
