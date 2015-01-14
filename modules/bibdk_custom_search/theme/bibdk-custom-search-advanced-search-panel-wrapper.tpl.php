e1db595767bb7940b4d36f2db648ce2096eb935b
Martin Møller <mmj@dbc.dk>
2015-01-14 09:16:14 +0100

    changed names of id's

--- modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
@@ -1,14 +1,14 @@
-<div id="search-advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="clearfix <?php print $form['#toggled']; ?>">
+<div id="custom-search--advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="clearfix <?php print $form['#toggled']; ?>">
   <a id="selid_custom_search_expand" class="text-darkgrey" href="#">
     <span class="toggle-text <?php print $form['#expand_hidden']; ?>">
-      <svg class="custom-search--expand-toggle-icon">
+      <svg class="custom-search--advanced-toggle-icon">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-plus-circle"></use>
       </svg>
       <?php print t('Expand search options'); ?>
     </span>
 
     <span class="toggle-text <?php print $form['#collaps_hidden']; ?>">
-      <svg class="custom-search--expand-toggle-icon">
+      <svg class="custom-search--advanced-toggle-icon">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-minus-circle"></use>
       </svg>
       <?php print t('Collapse search options'); ?>
