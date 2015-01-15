3e50db1e713291f65675fe6ff417879b4e6eb18a
Martin Møller <mmj@dbc.dk>
2015-01-15 10:55:57 +0100

    - re-organized html - disabled fieldset description

--- modules/bibdk_custom_search/theme/bibdk_custom_search-search-element-form.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk_custom_search-search-element-form.tpl.php
@@ -4,19 +4,24 @@
  * Theme implementation for bib.dk custom search elements.
  */
 ?>
-<div class='fieldset-legend'><label for="<?php print $element_id; ?>"><?php print $title; ?></label></div>
-<div class="fieldset-description"><?php print $description;?></div>
-<p class="helptext popover-button" title="<?php print strip_tags($help); ?>">
-  <a href="#" aria-label="<?php print t('Help', array(), array('context' => 'bibdk_theme')); ?>" role="button">?</a>
-</p>
-<div class="popover element-wrapper linkme-wrapper visuallyhidden hide-text">
-  <p class="user-msg ">
-    <?php print $help; ?>
+<div class='fieldset-legend'>
+  <label for="<?php print $element_id; ?>"><?php print $title; ?></label></div>
+<!--<div class="fieldset-description">--><?php //print $description;?><!--</div>-->
+<div class="custom-search--helptext-container">
+  <p class="helptext popover-button" title="<?php print strip_tags($help); ?>">
+    <a href="#" aria-label="<?php print t('Help', array(), array('context' => 'bibdk_theme')); ?>" role="button">?</a>
   </p>
-  <a href="#" aria-label="<?php print t('Close help', array(), array('context' => 'bibdk_theme')); ?>" 
-     role="button" class="close icon icon-left icon-red-x "> 
-  </a>
+
+  <div class="popover element-wrapper linkme-wrapper visuallyhidden hide-text">
+    <p class="user-msg ">
+      <?php print $help; ?>
+    </p>
+    <a href="#" aria-label="<?php print t('Close help', array(), array('context' => 'bibdk_theme')); ?>"
+       role="button" class="close icon icon-left icon-red-x ">
+    </a>
+  </div>
 </div>
+
 <div class="bibdk-custom-search-element">
   <?php print drupal_render_children($form); ?>
 </div>
