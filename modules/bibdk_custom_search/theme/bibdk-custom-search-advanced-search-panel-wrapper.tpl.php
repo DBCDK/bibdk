6d54318b723e9ddc9c855a3976a4f07b3a161e72
Martin Møller <mmj@dbc.dk>
2015-01-16 16:07:01 +0100

    search pages styling

--- modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
@@ -1,23 +1,26 @@
-<div id="custom-search--advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="<?php print $form['#toggled']; ?>">
+<div id="custom-search--advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="<?php print $form['#toggled']; ?> show-for-large-up">
   <a id="selid_custom_search_expand" class="text-darkgrey" href="#">
     <span class="toggle-text <?php print $form['#expand_hidden']; ?>">
       <svg class="custom-search--advanced-toggle-icon">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-plus-circle"></use>
       </svg>
-      <?php print t('Expand search options'); ?>
+      <span class="custom-search--advanced-toggle-text">
+        <?php print t('Expand search options'); ?>
+      </span>
     </span>
 
     <span class="toggle-text <?php print $form['#collaps_hidden']; ?>">
       <svg class="custom-search--advanced-toggle-icon">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-minus-circle"></use>
       </svg>
-      <?php print t('Collapse search options'); ?>
+      <span class="custom-search--advanced-toggle-text">
+        <?php print t('Collapse search options'); ?>
+      </span>
     </span>
 
   </a>
 </div>
-<!--TODO mmj remove below debug outcomment -->
-<div id="search-advanced" class="<?php #print $form['#collaps_hidden']; ?>">
+<div id="search-advanced" class="<?php print $form['#collaps_hidden']; ?> show-for-large-up">
   <div id="search-advanced-panel">
     <!-- ADVANCED SEARCH-->
     <?php print drupal_render_children($form); ?>
