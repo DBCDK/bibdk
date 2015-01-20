cb70d1890e3353ccc1cc5eeb74bcaa8a28c20f41
Martin Møller <mmj@dbc.dk>
2015-01-20 15:49:28 +0100

    Fixed error related to bibdk_modal conversion

--- modules/bibdk_custom_search/js/bibdk_custom_search_list.js
+++ modules/bibdk_custom_search/js/bibdk_custom_search_list.js
@@ -1,12 +1,13 @@
 (function($) {
   var CustomSearchList = {
+    selector: '',
     move: function(element) {
       var from = $(element).attr('data-action-move-from');
       var to = $(element).attr('data-action-move-to');
       $('#' + from + ' option:selected').each(function(i, option) {
         var label = $(option).text();
         var value = $(option).val()
-        $('#' + to).append('<option value="' + value + '">' + label + '</option>')
+        $('#' + to).append('<option value="' + value + '">' + label + '</option>');
         $('#' + from + ' option[value=' + value + ']').remove();
       });
     },
@@ -17,25 +18,25 @@
         var value = $(option).val();
         var label = $(option).html();
         if($('input[value=' + value + ']').length == 0) {
-          var checkbox = '<div class="form-item form-type-checkbox"><input class=" form-checkbox" type="checkbox" checked="true" name="' + term_type_wrapper + '[' + value + ']" value="' + value + '" tabindex="0">  <label class="option">' + label + '</label></div>'
-          $(element).closest('.custom-search-options-list').before(checkbox);
+          var checkbox = '<div class="form-item form-type-checkbox"><input class=" form-checkbox" type="checkbox" checked="true" name="' + term_type_wrapper + '[' + value + ']" value="' + value + '" tabindex="0">  <label class="option">' + label + '</label></div>';
+          $(CustomSearchList.selector).before(checkbox);
         }
       });
-      //$('.custom-search-list-close').trigger('click');
+      CustomSearchList.close();
     },
-    close: function(id) {
+    close: function() {
       jQuery('a.close-reveal-modal').trigger('click');
     },
+
     open: function(id) {
-      $('.popover').addClass('visuallyhidden');
-      $(id).removeClass('visuallyhidden').find('input').select();
+      CustomSearchList.selector = '#search-advanced-panel *[data-id="' + id + '"]';
     }
   };
   /** Move element from one select to another */
   Drupal.behaviors.custom_search_list = {
     attach: function(context) {
       $('.popover-button-list', context).click(function(e) {
-        CustomSearchList.open('#' + $(this).attr('data-id'));
+        CustomSearchList.open($(this).attr('data-id'));
       });
       $('.custom-search-list-action', context).click(function(event) {
         event.preventDefault();
