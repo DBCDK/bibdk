2d1f0b11c6e1ae302135085856bd227e36412c0d
Martin Møller <mmj@dbc.dk>
2015-01-21 12:54:54 +0100

    moved custom_search specific code from script.js to bibdk_custom_search.js

--- modules/bibdk_custom_search/js/bibdk_custom_search.js
+++ modules/bibdk_custom_search/js/bibdk_custom_search.js
@@ -20,9 +20,74 @@
     }
   };
 
+  var CustomSearch = {
+    toggleAdvancedSearchPanel: function(context){
+      // Toggle advanced search options
+      $('#custom-search--advanced-toggle', context).click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('toggled');
+        $('.toggle-text', this).toggleClass('hidden');
+        $('#search-advanced').toggleClass('hidden');
+        // Move to first input field in expanded search, if activated.
+        if($('#custom-search--advanced-toggle').hasClass('toggled') === true) {
+          $('#edit-advanced').removeClass('is-visible');
+          $('form#search-block-form input[name="search_block_form"]').not('.page-search form#search-block-form input[name="search_block_form"], .page-vejviser form#search-block-form input[name="search_block_form"]').focus();
+        }
+        else {
+          $('#edit-advanced').addClass('is-visible');
+          $('form#search-block-form').find('.bibdk-custom-search-element input[type=text], .bibdk-custom-search-element textarea').filter(':visible:first').focus();
+        }
+      });
+    },
+    handleSheetMusic: function(context){
+      // *************************** SHEET MUSIC *********************************************** //
+      $('#search-advanced .form-type-checkbox input').change(function() {
+        // clear other checkboxes if top-level default is selected.
+        if($(this).hasClass('default-value') && $(this).is(':checked') && $(this).val() == 'mu') {
+          $(this).closest(".bibdk-custom-search-element").find("input").each(function(i) {
+            if(!$(this).hasClass('default-value')) {
+              $(this).attr('checked', false);
+            }
+          })
+        }
+      });
+    },
+    splitInColumns: function(context){
+      // ***** Split radio elements in columns ***** //
+      var count = 0;
+      var column = 0;
+      var columns = [];
+      $('fieldset.noder #search-advanced .form-radios').children().each(function() {
+        if($(this).hasClass('column')) {
+          // means it's called a second time
+          return false;
+        }
+        if($(this).hasClass('form-type-radio')) {
+          count++;
+          column = Math.ceil(count / 5) - 1;
+        }
+        if(!columns[column]) {
+          columns[column] = document.createElement("div");
+          $(columns[column]).addClass('column');
+          $(columns[column]).addClass('column' + column);
+          $(columns[column]).addClass('form-wrapper');
+        }
+        $(columns[column]).append($(this));
+      });
+      columns.forEach(appendColumn);
+
+      function appendColumn(elem) {
+        $('fieldset.noder #search-advanced .form-radios').append(elem);
+      }
+    }
+  };
+
   Drupal.behaviors.bibdk_custom_search = {
     attach: function(context) {
       getAdvancedSearchPanel();
+      CustomSearch.toggleAdvancedSearchPanel(context);
+      CustomSearch.handleSheetMusic(context);
+      CustomSearch.splitInColumns(context);
     }
   };
 }(jQuery));
