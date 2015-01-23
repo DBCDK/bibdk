01ea2b28c5ddbf55c62d8e9cfa9cd16ffa106217
Martin Møller <mmj@dbc.dk>
2015-01-23 11:04:58 +0100

    cleanup

--- modules/bibdk_openuserstatus/theme/bibdk_openuserstatus_return_date.tpl.php
+++ modules/bibdk_openuserstatus/theme/bibdk_openuserstatus_return_date.tpl.php
@@ -1,6 +1,6 @@
 <span><?php print $date; ?></span>
 <span class="<?php print $alert_class; ?>" <?php print $title; ?>>
-   <?php if ($renewable) : ?>
-     <p><?php print $renewable; ?></p>
-   <?php endif; ?>
-  </span>
+  <?php if ($renewable) : ?>
+    <p><?php print $renewable; ?></p>
+  <?php endif; ?>
+</span>
