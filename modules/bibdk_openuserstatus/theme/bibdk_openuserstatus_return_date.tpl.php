403bdecb1318e6edaad8407f225d27ad07cb9ee1
Martin Møller <mmj@dbc.dk>
2015-01-23 11:04:29 +0100

    refactored/cleaned up template

--- modules/bibdk_openuserstatus/theme/bibdk_openuserstatus_return_date.tpl.php
+++ modules/bibdk_openuserstatus/theme/bibdk_openuserstatus_return_date.tpl.php
@@ -1,18 +1,6 @@
-<?php
-if ($overdue) {
-  $date_overdue = t('Delivery_date_overdue', array(), array('context' => 'bibdk_openuserstatus'));
-  $alert_class = 'red-alert';
-  $title_attr = 'title="' . $date_overdue . '"';
-}
-else {
-  $alert_class = 'no-alert';
-  $title_attr = '';
-}
-?>
-
 <span><?php print $date; ?></span>
-<span class="<?php print $alert_class; ?>" <?php print $title_attr; ?>>
+<span class="<?php print $alert_class; ?>" <?php print $title; ?>>
    <?php if ($renewable) : ?>
-     <?php print '<p>' . $renewable . '</p>'; ?>
+     <p><?php print $renewable; ?></p>
    <?php endif; ?>
   </span>
