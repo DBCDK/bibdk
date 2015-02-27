7aff241c61e99b47cd523eeeb86f33e0ff6a30a8
pjohans <pjohans@gmail.com>
2015-02-27 12:12:04 +0100

    pass a text to show if no reviews are found

--- modules/bibdk_voxb/js/bibdk_voxb_load.js
+++ modules/bibdk_voxb/js/bibdk_voxb_load.js
@@ -11,6 +11,13 @@
     else {
       var element = $('.bibdk_voxb_tab[data-pid="' + voxb.pid + '"]');
       element.html(voxb.markup);
+
+        if(voxb.no_review_txt != 'NONE'){
+            var title = element.closest('.tab-item').find('[id^="selid-worktab-voxb"]');
+            console.log(title);
+            title.html(voxb.no_review_txt);
+         }
+
       // attach behaviours to stay in context
       Drupal.attachBehaviors(element);
       Bibdk_voxb.voxb_settings.init(voxb);
