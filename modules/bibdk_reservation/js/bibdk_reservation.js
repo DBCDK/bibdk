7021e289ee46b936177735c37865e5c5f8a02011
Anders-Andersen <ana@dringo.dbc.dk>
2015-03-04 15:47:52 +0100

    feature/1549_double_order_confirmbox

--- modules/bibdk_reservation/js/bibdk_reservation.js
+++ modules/bibdk_reservation/js/bibdk_reservation.js
@@ -42,7 +42,7 @@
   //Confirm box for id's already ordered in reservation step 2
   Drupal.behaviors.alreadyOrder = {
     attach: function(context, settings) {
-      $('.edit-needbeforedate').ready(function(){
+      $('.edit-needbeforedate').once().ready(function(){
           if (Drupal.settings.alreadyOrder.alert_already_showed) {
             var r = confirm(Drupal.settings.alreadyOrder.alert_message);
             if ( r == false ) {
