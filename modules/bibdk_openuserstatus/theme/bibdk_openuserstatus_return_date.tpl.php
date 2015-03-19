94355590d2910da0e369000998b7dfa71c9c0a87
pjohans <pjohans@gmail.com>
2015-03-19 15:42:14 +0100

    renewable message on seperat line

--- modules/bibdk_openuserstatus/theme/bibdk_openuserstatus_return_date.tpl.php
+++ modules/bibdk_openuserstatus/theme/bibdk_openuserstatus_return_date.tpl.php
@@ -1,6 +1,6 @@
 <span class="userstatus--date"><?php print $date; ?></span>
-<span class="<?php print $alert_class; ?>" <?php print $title; ?>>
+<span class="<?php print $alert_class; ?>" <?php print $title; ?>></span>
   <?php if ($renewable) : ?>
     <p><?php print $renewable; ?></p>
   <?php endif; ?>
-</span>
+
