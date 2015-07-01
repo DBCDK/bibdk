732cd6e6b8c3e9f782209db585c80fec2d531f9b
pjohans <pjohans@gmail.com>
2015-07-01 15:07:05 +0200

    a lot of changes on the facetbrowser and other modules

--- modules/bibdk_facetbrowser/theme/facetbrowser-term-names.tpl.php
+++ modules/bibdk_facetbrowser/theme/facetbrowser-term-names.tpl.php
@@ -1,11 +1,16 @@
 
 
-<?php if(!$selected_facets):?>
+<?php //if(!$selected_facets):
+if(TRUE):
+?>
 <div class="expand" data-expand="more"><span class="icon icon-left icon-blue-plus">+</span><span><?php echo  t('label_facet_show_more', array(), array('context' => 'bibdk_facetbrowser')); ?></span></div>
 <div class="expand" data-expand="less"><span class="icon icon-left icon-blue-minus">-</span><span><?php echo  t('label_facet_show_less', array(), array('context' => 'bibdk_facetbrowser')); ?></span></div>
 <?php endif ?>
 
-<div class="select" data-expand="select" data-reveal-id="<?php echo $facet_id; ?>"><span class="icon icon-left icon-blue-down">+</span><span><?php echo  t('label_facet_select_unselect', array(), array('context' => 'bibdk_facetbrowser')); ?></span></div>
+<!--<div class="select" data-expand="select" data-reveal-id="bibdk-modal"><span class="icon icon-left icon-blue-down">+</span><span><?php echo  t('label_facet_select_unselect', array(), array('context' => 'bibdk_facetbrowser')); ?></span></div>-->
+
+<!--<a data-reveal-ajax="true" data-reveal-id="bibdk-modal" href="/postgres/bibdk_modal/facetgroup">hest</a>-->
+
 
 
 
