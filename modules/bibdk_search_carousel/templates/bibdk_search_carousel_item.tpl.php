119fc574b7558d025ef25c50d50aca1503179d08
JrgenGNielsen <jgn@dbc.dk>
2015-07-29 13:46:57 +0200

    use url() to set language prefix

--- modules/bibdk_search_carousel/templates/bibdk_search_carousel_item.tpl.php
+++ modules/bibdk_search_carousel/templates/bibdk_search_carousel_item.tpl.php
@@ -6,17 +6,17 @@
 
 <div class="carousel-item">
   <div class="carousel-item-image">
-    <a href="search/work/rec.id=<?php echo urlencode($pid); ?>" title="<?php print check_plain($title); ?>">
+    <a href="<?php echo $url; ?>" title="<?php print check_plain($title); ?>">
       <img src="<?php echo $image; ?>" alt=""/>
     </a>
   </div>
   <div class="carousel-item-title">
-    <a href="search/work/rec.id=<?php echo urlencode($pid); ?>">
+    <a href="<?php echo $url; ?>">
       <?php print check_plain($title); ?>
     </a>
   </div>
   <div class="carousel-item-creator">
-    <a href="search/work/rec.id=<?php echo urlencode($pid); ?>">
+    <a href="<?php echo $url; ?>">
       <?php print check_plain($creator); ?>
     </a>
   </div>
