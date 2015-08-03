fe2e95f92f229139a622a6d08ee6834e24285cb2
pjohans <pjohans@gmail.com>
2015-08-03 14:15:22 +0200

    fix order of checkboxes

--- modules/bibdk_facetbrowser/theme/facetbrowser-filter-form-element.tpl.php
+++ modules/bibdk_facetbrowser/theme/facetbrowser-filter-form-element.tpl.php
@@ -11,16 +11,16 @@
         <?php $selected = (isset($element['#selected_facets'][$name]) && $element['#selected_facets'][$name] == $element['#facet_name']); ?>
         <?php $deselected = (isset($element['#deselected_facets'][$name]) && $element['#deselected_facets'][$name] == $element['#facet_name']); ?>
 
-        <p class="checkbox-element-select">
-          <input id="<?php print $element['#facet_key']; ?>-<?php print $name; ?>"
-                 class="form-checkbox form-item" type="checkbox" <?php ($selected) ? print 'checked' : print '';?> value=1
-                 name="<?php print $element['#facet_key'] . '[' . $name . '][]';?>">
-        </p>
-
         <p class="checkbox-element-deselect">
           <input id="<?php print $element['#facet_key']; ?>-<?php print $name; ?>"
                  class="form-checkbox form-item" type="checkbox" <?php ($deselected) ? print 'checked' : print '';?> value=0
                  name="<?php print $element['#facet_key'] . '[' . $name . '][]';?>" data-deselect="true"">
+        </p>
+
+        <p class="checkbox-element-select">
+          <input id="<?php print $element['#facet_key']; ?>-<?php print $name; ?>"
+                 class="form-checkbox form-item" type="checkbox" <?php ($selected) ? print 'checked' : print '';?> value=1
+                 name="<?php print $element['#facet_key'] . '[' . $name . '][]';?>">
         </p>
 
       </div>
