0108d2ad6db9dcddd1a15783bead071cf0450908
JgenGNielsen <jgn@dbc.dk>
2015-11-26 16:08:04 +0100

    move render array to .module file

--- modules/bibdk_subject_hierarchy/theme/bibdk-subject-hierarchy-item.tpl.php
+++ modules/bibdk_subject_hierarchy/theme/bibdk-subject-hierarchy-item.tpl.php
@@ -19,27 +19,11 @@
 
   <div class="subjects-sublist">
     <ul>
-      <?php foreach ($variables['hierarchy']['term'] as $key => $item) {
-        $link = array(
-            '#theme' => 'link',
-            '#text' => $item['ord'],
-            '#path' => NULL,
-            '#options' => array('attributes' => array()),
-            '#prefix' => '<li>',
-            '#suffix' => '</li>'
-        );
-        if (!empty($item['cql']) && empty($item['term'])) {
-            $link['#path'] = 'search/' . $variables['search_path'];
-            $link['#options']['query'] = array('search_block_form' => trim($item['cql']));
-            $link['#options']['fragment'] = 'content';
-            $link['#options']['html'] = TRUE;
+      <?php
+        foreach ($variables['hierarchy'] as $key => $item) {
+            echo render($item);
         }
-        else {
-            $link['#path'] = 'bibdk_subject_hierarchy/nojs/' . $variables['current_key'] . ',' . $key;
-            $link['#options']['attributes']['class'] = array('use-ajax', 'nesting');
-        }
-        echo render($link);
-      } ?>
+      ?>
     </ul>
   </div>
 
