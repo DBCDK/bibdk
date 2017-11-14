dd9292e719cc494abd160e81553c5cb0b620f21d
pjohans <pjohans@gmail.com>
2017-11-14 11:25:10 +0100

    use once() + error handling

--- modules/bibdk_heimdal/js/bibdk_heimdal_email_select.js
+++ modules/bibdk_heimdal/js/bibdk_heimdal_email_select.js
@@ -1,35 +1,37 @@
-(function($) {
+(function ($) {
   var bibdk_heimdal = {};
 
-  bibdk_heimdal.getEmail = function(modal) {
-    console.log('FISK');
+  bibdk_heimdal.getEmail = function (modal) {
     var url = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'bibdk_modal/heimdal/verify';
     var request = $.ajax({
       url: url,
       type: 'GET',
-      success: function(data) {
+      success: function (data) {
         modal.html(data);
         modal.foundation('reveal', 'open');
+        // make modal stay open
+        modal.attr("data-options", "close_on_background_click:false;close_on_esc:false");
       },
-          error:function(data){
-        console.log(data);
+      error: function (data) {
+        var options = [];
+        options['context'] = 'heimdal';
+        alert(Drupal.t('Something went wrong, please try again', [], options));
       }
     });
   };
 
-  bibdk_heimdal.openModal = function(){
+  bibdk_heimdal.openModal = function () {
     // get the modal
     var modal = $('#bibdk-modal');
-    // make modal stay open
-    modal.attr("data-options","close_on_background_click:false;close_on_esc:false");
     bibdk_heimdal.getEmail(modal);
   };
 
 
-
   Drupal.behaviors.bibdk_heimdal = {
-    attach: function(context, settings) {
-      bibdk_heimdal.openModal();
+    attach: function (context, settings) {
+      $('#bibdk-modal',context).once('fisk', function(){
+        bibdk_heimdal.openModal();
+      })
     }
   };
 }(jQuery));
