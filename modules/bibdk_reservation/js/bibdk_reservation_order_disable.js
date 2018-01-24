35170761bb208641d3a1a824988426f704d71008
Jørgen G. Nielsen <jgn@dbc.dk>
2018-01-24 13:48:28 +0100

    disable order button when form submitted

--- modules/bibdk_reservation/js/bibdk_reservation_order_disable.js
+++ modules/bibdk_reservation/js/bibdk_reservation_order_disable.js
@@ -1,14 +1,20 @@
 
 // bibdk_reservation_order_disable.js
-// in order reservation step 3 - disable order button when click - to prevent multiorders
-//
+// in order reservation step 2 - disable order button when click - to prevent multiorders
 
 (function($) {
-  //disable order button after in reservation next step 4
+  // Disable order button after in reservation next step 2.
   Drupal.behaviors.alreadyOrder = {
     attach: function() {
-      $('#bibdk-reservation-create-wizard-form').submit(function(){
-        $('#edit-next').prop('disabled', true);
+      $('#bibdk-reservation-create-wizard-form').submit(function(event){
+        if ($(this).hasClass('submitted')) {
+          event.preventDefault();
+        }
+        else {
+          $(this).addClass('submitted');
+          $('#edit-next').parent().append('<div class="ajax-progress ajax-progress-throbber"><div class="throbber"></div></div>');
+          $('#edit-navigation input[type="submit"]').css({"background-color": 'grey', 'pointer-events': 'none'});
+        }
       });
     }
   };
