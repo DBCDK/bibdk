12ee5b661d4f72aabc7c581f1f68ac9c855300cf
Jakob Larsen <jakob@revealit.dk>
2018-06-29 10:08:14 +0200

    2507 Small frontend changes for consent styles

--- modules/bibdk_gdpr/js/bibdk_gdpr.consent.js
+++ modules/bibdk_gdpr/js/bibdk_gdpr.consent.js
@@ -11,22 +11,25 @@
       modal.foundation('reveal', 'open');
       // make modal stay open
       modal.attr("data-options", "close_on_background_click:false;close_on_esc:false");
-      modal.find('.message').hide();
+      modal.find('.consent-message').hide();
       modal.find('.see-more').click(function(event) {
         $(this).toggleClass('open');
         if ($(this).hasClass('open')) {
           $(this).text(Drupal.t('See less', {}, {context: 'bibdk_gdpr'}));
-          modal.find('.message').slideDown();
+          modal.find('.consent-message').slideDown();
         }
         else {
           $(this).text(Drupal.t('See more', {}, {context: 'bibdk_gdpr'}));
-          modal.find('.message').slideUp();
+          modal.find('.consent-message').slideUp();
         }
       });
       modal.find('button').click(function() {
         if (modal.find('input:checked').length) {
           $.post(Drupal.settings.basePath + 'bibdk-gdpr-consent', {consent: 1});
           modal.foundation('reveal', 'close');
+        }
+        else {
+          modal.find('.form-type-checkbox').addClass('error');
         }
         return false;
       });
