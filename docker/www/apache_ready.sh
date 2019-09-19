533a6a5efbabd375e95c8b6c1fe6ba4365255d9a
pjohans <pjohans@gmail.com>
2019-09-19 08:17:57 +0200

    fixed an error

--- docker/www/apache_ready.sh
+++ docker/www/apache_ready.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 STATUS=$(curl -sI http://localhost | grep HTTP | cut -d' ' -f2)
-EMPTY=$(curl -sI http://localhost | wc -l)
 count=1
-while [ $EMPTY > 0  ]  || [ $STATUS !=  200 ]
+while [ -z "$STATUS" ] || [ "$STATUS" -ne "200" ]
 do
   count=$(expr $count + 1)
-  EMPTY=$(curl -sI http://localhost | wc -l)
   STATUS=$(curl -sI http://localhost | grep HTTP | cut -d' ' -f2)
-  if [ $count > 100 ]
+  if [ $count -gt "200" ]
   then
     exit 1
   fi
