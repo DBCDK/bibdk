3378a2fcb94f5dc84453920991e227de59429502
pjohans <pjohans@gmail.com>
2019-10-07 13:46:46 +0200

    added start of janitor

---
+++ jenkins/janitor/parse_git_repos.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# check if given string containts the substring feature
+is_feature(){
+if [[ "$1" =~ "feature" ]]; then
+return 0
+fi
+return 1
+}
+
+# get an array of feature names
+get_revlist(){
+features=[]
+revlist=$(git ls-remote --heads)
+for word in $revlist
+do
+if is_feature $word
+then
+featurename=$(echo $word | cut -d/ -f4)
+features=("${features[@]}" ${featurename/_/-})
+fi
+done
+echo ${features[2]}
+}
+
+get_kubelist(){
+
+}
+
+get_revlist
+
