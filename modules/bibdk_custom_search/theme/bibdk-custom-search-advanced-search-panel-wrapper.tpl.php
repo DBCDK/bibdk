6895f79a514c2da57621ffa27503a270339b5f28
Lars Hansen <lkh@dbc.dk>
2019-12-11 12:59:55 +0100

    The advanced search panel needs to be closed on load

--- modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
+++ modules/bibdk_custom_search/theme/bibdk-custom-search-advanced-search-panel-wrapper.tpl.php
@@ -1,5 +1,13 @@
-<div id="custom-search--advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class="<?php print $form['#toggled']; ?> show-for-large-up">
+<div id="custom-search--advanced-toggle" data-toggle-state-hidden="<?php print $form['#toggled']; ?>" class=" <?php print $form['#toggled']; ?> show-for-large-up">
   <a id="selid_custom_search_expand" class="text-darkgrey" href="#">
+    <span class="toggle-text <?php print $form['#collaps_hidden']; ?>">
+      <svg class="custom-search--advanced-toggle-icon">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-blue-minus-circle"></use>
+      </svg>
+      <span class="custom-search--advanced-toggle-text">
+        <?php print t('Collapse search options'); ?>
+      </span>
+    </span>
     <span class="toggle-text <?php print $form['#expand_hidden']; ?>">
       <svg class="custom-search--advanced-toggle-icon">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-blue-plus-circle"></use>
@@ -9,18 +17,9 @@
       </span>
     </span>
 
-    <span class="toggle-text <?php print $form['#collaps_hidden']; ?>">
-      <svg class="custom-search--advanced-toggle-icon">
-        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-blue-minus-circle"></use>
-      </svg>
-      <span class="custom-search--advanced-toggle-text">
-        <?php print t('Collapse search options'); ?>
-      </span>
-    </span>
-
   </a>
 </div>
-<div id="search-advanced" class="<?php print $form['#collaps_hidden']; ?> show-for-large-up">
+<div id="search-advanced" class="hidden <?php print $form['#collaps_hidden']; ?> show-for-large-up">
   <div id="search-advanced-panel">
     <!-- ADVANCED SEARCH-->
     <?php print drupal_render_children($form); ?>
