8a7599c8e14ec7f80220fe0edb7c5e671cf25b39
pjo <pjo@dbc.dk>
2020-02-28 15:42:34 +0100

    remove viewport - it is set in gultask

--- modules/bibdk_recommender/theme/bibdk-recommender-svg.tpl.php
+++ modules/bibdk_recommender/theme/bibdk-recommender-svg.tpl.php
@@ -1,6 +1,6 @@
 <?php print drupal_render($variables['title_creator']['material_biblio']) ?>
 <span class="svg-icon">
-    <svg class="media-<?php print $material_type; ?>" title="" alt="" viewbox="0 0 50 50">
+    <svg class="media-<?php print $material_type; ?>" title="" alt="">
       <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-media-<?php print $material_type ?>" ></use>
     </svg>
   </span>
