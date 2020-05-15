94fc76e154d014b0b17b44de8a3f578996b987b4
pjo <pjo@dbc.dk>
2020-05-15 15:18:09 +0200

    changes from review

--- docker/www/script/entry.sh
+++ docker/www/script/entry.sh
@@ -22,12 +22,12 @@ if [ "$1" == '' ]; then
 	# Make sure Apache shuts down properly
 	trap stop SIGINT SIGTERM
 
-cd /tmp || return
-tar -xf files.tar.gz
-rm -rf /var/www/html/sites/default/files/*
-cp -Rf files /var/www/html/sites/default
-chown -Rf www-data:www-data /var/www/html/sites/default/files
-rm -rf files files.tar.gz
+  cd /tmp || return
+  tar -xf files.tar.gz
+  rm -rf /var/www/html/sites/default/files/
+  cp -Rf files /var/www/html/sites/default
+  chown -Rf www-data:www-data /var/www/html/sites/default/files
+  rm -rf files files.tar.gz
 
 	# Check if external storage is mounted
 	#if [ -d '/data/files' ]; then
