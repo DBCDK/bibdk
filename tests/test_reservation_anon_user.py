5950e129aba7a0e2f31cb47db348872d7a17688d
Rikke Lose <rikkelose@172-17-33-4.dhcpaddr.dbc.dk>
2020-08-25 10:28:29 +0200

    add doc + tests

--- tests/test_reservation_anon_user.py
+++ tests/test_reservation_anon_user.py
@@ -124,3 +124,132 @@ class TestReservationAnonUser(helpers.BibdkUnitTestCase):
         )
         if not 'bestilling er modtaget' in message.text:
             assert False
+
+    def test_reservation_request_other_manifestation(self):
+        browser = self.browser
+        self._goto_frontpage()
+        wait = WebDriverWait(browser, 30)
+        self._check_pop_up()
+        # Perform search
+        input = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.CLASS_NAME, "search-block-form"
+                )
+            )
+        )
+
+        input.send_keys('svenskevældets fald aakjær')
+
+        submit = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.ID, "edit-submit"
+                )
+            )
+        )
+        submit.click()
+
+        # Open Reservation
+        reservation = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.ID, "any_edition_but_800010katalog99122907984205763"
+                )
+            )
+        )
+        reservation.click()
+
+        # Order material
+        order = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.ID, "any_edtion_order_800010katalog99122907984205763"
+                )
+            )
+        )
+        order.click()
+
+        # wait for the PopUpWindow to visible
+        wait.until(
+            self.found_window('PopUpWindowreservation')
+        )
+        # ensure we have two windows available
+        self.assertEqual(2, len(browser.window_handles))
+
+        # Check that a manifestation can be ordered
+        search_library = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.ID, "edit-anyfield"
+                )
+            )
+        )
+
+    def test_reservation_no_request_button(self):
+        browser = self.browser
+        self._goto_frontpage()
+        wait = WebDriverWait(browser, 30)
+        self._check_pop_up()
+        # Perform search
+        input = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.CLASS_NAME, "search-block-form"
+                )
+            )
+        )
+
+        input.send_keys('svenskevældets fald aakjær')
+
+        submit = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.ID, "edit-submit"
+                )
+            )
+        )
+        submit.click()
+
+        # Toggle work
+        work = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.ID, "800010katalog99122907984205763"
+                )
+            )
+        )
+        work.click()
+
+        # Check that first manifestation has no request button
+        try:
+            first = wait.until(
+                EC.visibility_of_element_located(
+                    (
+                        By.ID, "this_edition_800010-katalog:99122907984205763"
+                    )
+                )
+            )
+            not_found = False
+        except:
+            not_found = True
+        assert not_found
+
+        # Expand all manifestations
+        all = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.CLASS_NAME, "manifestation-toggle-link"
+                )
+            )
+        )
+        all.click()
+
+        # Check that second manifestation has order button
+        second = wait.until(
+            EC.visibility_of_element_located(
+                (
+                    By.ID, "this_edition_820030-katalog:512230"
+                )
+            )
+        )
