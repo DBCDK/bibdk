1ebae9c38eeba130c3e611357cc29e4df6a3fa54
pjo <pjo@dbc.dk>
2020-09-08 12:48:17 +0200

    new siteid for staging bibliotek.dk

--- modules/bibdk_matomo/piwik/piwik_no_cookies.js
+++ modules/bibdk_matomo/piwik/piwik_no_cookies.js
@@ -10,7 +10,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
   var u="//stats.dbc.dk/";
   _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', 11]);
+  _paq.push(['setSiteId', 32]);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
