64ebd09281a8c516232bc359937313c2852c234d
Lars Krogsgård Hansen <lkh@dbc.dk>
2021-01-07 15:26:03 +0100

    Trying to use the branchId instead of the agencyId

--- modules/bibdk_favourite/js/bibdk_favourite.js
+++ modules/bibdk_favourite/js/bibdk_favourite.js
@@ -4,24 +4,23 @@
     $("[data-bibdk-favourite-more-info-toggler]", context).once("agency").click(function(e) {
       var self = $(this);
       var target = $(e.target);
-      if(!target.hasClass("bibdk-favourite--add-remove-library") &&
-        !target.hasClass("agencylist-link")) {
+      if(!target.hasClass("bibdk-favourite--add-remove-library") && !target.hasClass("agencylist-link")) {
         self.next(".bibdk-favourite--more-info").toggleClass("is-toggled");
         self.next(".bibdk-favourite--more-info-toggle-indicator").toggleClass("is-toggled");
       }
     });
   }
 
-  function setClickListeners(context){
-    $('.bibdk-favourite--add-remove-library', context).click(function(e){
+  function setClickListeners(context) {
+    $(".bibdk-favourite--add-remove-library", context).click(function(e) {
       window.Foundation.reloadPageOnModalClose = 1;
     });
   }
 
   Drupal.behaviors.bibdk_favourite = {
     attach: function(context) {
-      setClickListeners(context);
       addTogglers(context);
+      setClickListeners(context);
     }
   };
 }(jQuery));
