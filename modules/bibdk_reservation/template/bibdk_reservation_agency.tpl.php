f7c31b69911a6dd9de1195889fc0516fa36c5da7
Lars Krogsgård Hansen <lkh@dbc.dk>
2021-02-10 19:19:30 +0100

    The double zipcode city is gone

--- modules/bibdk_reservation/template/bibdk_reservation_agency.tpl.php
+++ modules/bibdk_reservation/template/bibdk_reservation_agency.tpl.php
@@ -5,7 +5,7 @@
     <?php print $shortname; ?><br/>
   <?php endif; ?>
   <?php if (isset($address)) : ?>
-    <?php print $address; ?><br/>
+    <?php print $address; ?>
     <?php print $postalCode; ?> <?php print $city; ?><br/>
   <?php endif; ?>
     <?php if (isset($subdivision)) :?>
